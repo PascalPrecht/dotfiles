79ea2076f6ee7c5e69191ac40215dfc33c25baaf
Pascal Precht <pascal.precht@googlemail.com>
2012-10-27 21:40:24 +0200

    Changed youtube script

--- .js/youtube.com.js
+++ .js/youtube.com.js
@@ -1,7 +1,1 @@
-/*$('#masthead-user').hide();
-$('#masthead-nav').hide();
-$('#masthead-search-bar-container').css('padding-right', '400px');
 $('.ad-div').hide();
-$('#video-sidebar').hide();
-$('#feed').css('width', '770');
-$('#feed-background').css('width', '770');*/
