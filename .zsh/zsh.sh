4471afa39eea7199e4d79c2321834ddc86f8b2bf
Pascal Precht <pascal.precht@googlemail.com>
2012-11-16 18:17:15 +0100

    Making pure.zsh-theme behave as fallback theme

--- .zsh/zsh.sh
+++ .zsh/zsh.sh
@@ -1,7 +1,11 @@
+source $ZSH_ENV/aliases.zsh
+
 # Loading the theme
 if [ ! "$ZSH_THEME" = "" ]
   then
   if [ -f "$ZSH_ENV/themes/$ZSH_THEME.zsh-theme" ]
     source "$ZSH_ENV/themes/$ZSH_THEME.zsh-theme"
+  else
+    source "$ZSH_ENV/themes/pure.zsh-theme"
   fi
 if
